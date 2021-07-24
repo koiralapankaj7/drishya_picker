8e7c06b9a7de3e37e10da393710534be1c04adf7
koiralapankajgit <koiralapankaj007@gmail.com>
2021-07-24 14:02:55 +0545

    permission updated

--- lib/src/gallery/src/controllers/gallery_repository.dart
+++ lib/src/gallery/src/controllers/gallery_repository.dart
@@ -39,52 +39,52 @@ class GalleryRepository {
     albumNotifier.value = const BaseState(isLoading: true);
     entitiesNotifier.value = const BaseState(isLoading: true);
 
-    final state = await PhotoManager.requestPermissionExtend();
-    if (state == PermissionState.authorized) {
-      try {
-        // final options = FilterOptionGroup()
-        //   ..setOption(
-        //     type.assetType,
-        //     const FilterOption(
-        //       needTitle: true,
-        //       sizeConstraint: SizeConstraint(ignoreSize: true),
-        //     ),
-        //   );
+    // final state = await PhotoManager.requestPermissionExtend();
+    // if (state == PermissionState.authorized) {
+    try {
+      // final options = FilterOptionGroup()
+      //   ..setOption(
+      //     type.assetType,
+      //     const FilterOption(
+      //       needTitle: true,
+      //       sizeConstraint: SizeConstraint(ignoreSize: true),
+      //     ),
+      //   );
 
-        final albums = await PhotoManager.getAssetPathList(type: type);
+      final albums = await PhotoManager.getAssetPathList(type: type);
 
-        // Update album list
-        albumsNotifier.value = BaseState(data: albums, hasPermission: true);
+      // Update album list
+      albumsNotifier.value = BaseState(data: albums, hasPermission: true);
 
-        final album = albums.isNotEmpty ? albums.first : null;
-        // Update selected album
-        albumNotifier.value = BaseState(data: album, hasPermission: true);
+      final album = albums.isNotEmpty ? albums.first : null;
+      // Update selected album
+      albumNotifier.value = BaseState(data: album, hasPermission: true);
 
-        final entities = await album?.assetList ?? <AssetEntity>[];
-        // Update selected album entities list
-        entitiesNotifier.value = BaseState(data: entities, hasPermission: true);
-      } catch (e) {
-        albumsNotifier.value = BaseState(
-          hasPermission: true,
-          hasError: true,
-          error: e.toString(),
-        );
-        entitiesNotifier.value = BaseState(
-          hasPermission: true,
-          hasError: true,
-          error: e.toString(),
-        );
-      }
-    } else {
-      albumsNotifier.value = const BaseState(
+      final entities = await album?.assetList ?? <AssetEntity>[];
+      // Update selected album entities list
+      entitiesNotifier.value = BaseState(data: entities, hasPermission: true);
+    } catch (e) {
+      albumsNotifier.value = BaseState(
+        hasPermission: false,
         hasError: true,
-        error: 'Permission denied',
+        error: e.toString(),
       );
-      entitiesNotifier.value = const BaseState(
+      entitiesNotifier.value = BaseState(
+        hasPermission: false,
         hasError: true,
-        error: 'Permission denied',
+        error: e.toString(),
       );
     }
+    // } else {
+    //   albumsNotifier.value = const BaseState(
+    //     hasError: true,
+    //     error: 'Permission denied',
+    //   );
+    //   entitiesNotifier.value = const BaseState(
+    //     hasError: true,
+    //     error: 'Permission denied',
+    //   );
+    // }
   }
 
   /// Get assets for specific [album]
